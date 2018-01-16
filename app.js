b9cc6d0dd79cae92a0c6b9c4dd3cc9f20cec3b00
Victor Chaplin <victorchaplin@outlook.com>
2018-01-16 21:45:31 -0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log('Starting app...');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('Note added successfully');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Note already exists');
+    }
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
